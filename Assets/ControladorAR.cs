aeeaeb3451131d13da60c93d64396d76f4d22e03
mony527 <m.jblasco@alumnos.upm.es>
2026-05-06 21:45:36 +0200

    Add logica inicial y activadores definitivos

--- Assets/ControladorAR.cs
+++ Assets/ControladorAR.cs
@@ -1,28 +1,115 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+
 
 public class Controlador : MonoBehaviour
 {
-    public GameObject panSecoO;
-    public GameObject lecheO;
-    public GameObject bandejaO;
-    public GameObject panMojadoO;
-    private bool panSeco, leche, bandeja, panMojado;
+    public GameObject panSeco3D;
+    public GameObject leche3D;
+    public GameObject bandeja3D;
+
+    public GameObject panMojado3D;
+    public GameObject bolHuevo3D;
+    public GameObject panRebozado3D;
+
+    public GameObject aceite3D;
+    public GameObject sarten3D;
+    public GameObject sartenLista3D;
+
+    public GameObject panFrito3D;
+
+    public GameObject azucar3D;
+    public GameObject canela3D;
+    public GameObject mezclaDulce3D;
+
+    public GameObject panDulce3D;
+
+    public GameObject plato3D;
+
+    private bool panSeco, leche, bandeja, panMojado, bolHuevo, panRebozado, aceite, sarten, sartenLista,panFrito, azucar, canela, mezclaDulce, panDulce, plato, torrija ;
 
 
     void Start()
     {
-        panSeco = leche = bandeja = panMojado = false;
+        panSeco = leche = bandeja = panMojado = bolHuevo = panRebozado = aceite = sarten = sartenLista = panFrito = azucar = canela = mezclaDulce = panDulce = plato = torrija = false;
+        
+        mezclaDulce3D.SetActive(false);
     }
 
     void Update()
+    {
+ 
+    }
+
+    public void Actualizar()
     {
         if (panSeco && leche && bandeja)
         {
             panMojado = true;
             panSeco = leche = false;
+
+            panSeco3D.SetActive(false);
+            leche3D.SetActive(false);
+            panMojado3D.SetActive(true);
+
+            panMojado3D.transform.SetParent(bandeja3D.transform);
+            panMojado3D.transform.localPosition = new Vector3(0, 1.0f, 0);
         }
+
+        if (panMojado && !bandeja)
+        {
+            panMojado=false;
+            panMojado3D.SetActive(false);
+
+            panSeco3D.SetActive(true);
+            leche3D.SetActive(true);
+        }
+
+        if(aceite && sarten) 
+        { 
+            aceite = false;
+            sarten = false;
+            sartenLista = true;
+
+            aceite3D.SetActive(false);
+            sarten3D.SetActive(false);
+            sartenLista3D.SetActive(true);
+        }
+
+        if(sartenLista && !sarten)
+        {
+            sartenLista = false;
+
+            aceite3D.SetActive(true);
+            sarten3D.SetActive(true);
+            sartenLista3D.SetActive(false);
+        }
+
+        if (azucar && canela) 
+        { 
+            azucar = false;
+            canela = false;
+            mezclaDulce = true;
+
+            azucar3D.SetActive(false);
+            canela3D.SetActive(false);
+            mezclaDulce3D.SetActive(true);
+            mezclaDulce3D.transform.localPosition = azucar3D.transform.localPosition;
+
+        }
+
+        if(mezclaDulce && !azucar)
+        {
+            mezclaDulce = false;
+
+            azucar3D.SetActive(true);
+            canela3D.SetActive(true);
+            mezclaDulce3D.SetActive(false);
+
+        }
+
     }
 
     public void ITPresente(string id)
@@ -39,21 +126,72 @@ public class Controlador : MonoBehaviour
         {
             bandeja = true;
         }
+        if(id == "bolHuevo") 
+        {
+            bolHuevo = true;
+        }
+        if(id == "aceite")
+        {
+            aceite = true;
+        }
+        if (id == "sarten")
+        {
+            sarten = true;
+        }
+        if (id == "azucar")
+        {
+            azucar = true;
+        }
+        if (id == "canela")
+        {
+            canela = true;
+        }
+        if (id == "plato")
+        {
+            plato = true;
+        }
+        Actualizar();
     }
 
     public void ITAusente(string id)
     {
         if (id == "leche")
         {
-            leche = true;
+            leche = false;
         }
         if (id == "panSeco")
         {
-            panSeco = true;
+            panSeco = false;
         }
-        if (id =="bandeja")
+        if (id == "bandeja")
         {
-            bandeja = true;
+            bandeja = false;
         }
+        if (id == "bolHuevo")
+        {
+            bolHuevo = false;
+        }
+        if (id == "aceite")
+        {
+            aceite = false;
+        }
+        if (id == "sarten")
+        {
+            sarten = false;
+        }
+        if (id == "azucar")
+        {
+            azucar = false;
+        }
+        if (id == "canela")
+        {
+            canela = false;
+        }
+        if (id == "plato")
+        {
+            plato = false;
+        }
+        Actualizar();
+     
     }
 }
